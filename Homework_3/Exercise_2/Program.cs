fd0492a2b9ce553963c5eb113573d2ea2d27dcd5
navid80 <naviddavoudi80@gmail.com>
2025-12-20 23:19:08 +0330

    Implementing the second exercise

---
+++ Homework_3/Exercise_2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Exercise_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Person> people = new List<Person>();
+
+            people.Add(new Student
+            {
+                Name = "Ali Miri",
+                Age = 20,
+                StudentId = "S123",
+                Major = "Computer Science"
+            });
+
+            people.Add(new Professor
+            {
+                Name = "Dr. Sara Amini",
+                Age = 45,
+                ProfessorId = "P456",
+                Subject = "Data Mining"
+            });
+
+            people.Add(new Student
+            {
+                Name = "Mina Ahmadi",
+                Age = 22,
+                StudentId = "S124",
+                Major = "Mathematics"
+            });
+
+            people.Add(new Professor
+            {
+                Name = "Reza Keshavarz",
+                Age = 52,
+                ProfessorId = "4125",
+                Subject = "Physics"
+            });
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person.GetDetails());
+            }
+        }
+    }
+}
---
+++ Homework_3/Exercise_2/model/Professor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    public class Professor : Person
+    {
+        public string ProfessorId { get; set; }
+
+        public string Subject { get; set; }
+
+        public override string GetDetails()
+        {
+            return $"Professor Name : {Name}, Age : {Age}, ProfessorId : {ProfessorId}, Subject : {Subject}";
+        }
+    }
+}
