f2984a2d965ade35a33b764c121066b19ba9c503
navid80 <naviddavoudi80@gmail.com>
2025-12-20 22:25:53 +0330

    Implementing the book exercise

---
+++ Homework_3/Exercise_1/interface/ILibrary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    public interface ILibrary
+    {
+        void AddBook(Book book);
+        void BorrowBook(string title);
+        void ReturnBook(string title);
+    }
+}
---
+++ Homework_3/Exercise_1/model/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        
+        public string Author { get; set; }
+        
+        public string ISBN { get; set; }
+
+        public bool IsAvailable { get; private set; }
+
+        public Book(string title, string author, string isbn)
+        {
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            IsAvailable = true;
+        }
+
+        public void Borrow()
+        {
+            if (!IsAvailable)
+                throw new InvalidOperationException("This Book Is Not Available!");
+            
+            IsAvailable = false;
+        }
+        
+        public void Return()
+        {
+            if (IsAvailable)
+                throw new InvalidOperationException("Book Was Not Borrowed.");
+
+            IsAvailable = true;
+        }
+    }
+}
